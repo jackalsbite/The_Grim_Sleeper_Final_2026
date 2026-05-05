85d6609ef2053c7447aecd4240750893bc470638
Elizabeth Serrano <eliza.serrano62@gmail.com>
2026-05-05 15:14:52 -0500

    Learning/Starting Physics

--- player.py
+++ player.py
@@ -124,6 +124,9 @@ class Player:
     def interact(self):
         pass
 
+    def _physics_process(self, delta: float) -> None:
+        pass
+    
     def draw(self):
         kn.renderer.draw(
             self.texture,
